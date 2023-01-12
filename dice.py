7ce5cc9c3ccdca3634ee445518a408e6838b5ea5
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-01-12 13:58:19 +0000

    added instructions

--- dice.py
+++ dice.py
@@ -1,14 +1,18 @@
 import random
 
 class Dice:
-
+  roll_id = 0
   def __init__(self, numDie, die):
     self.numDie = numDie
     self.die = die
-    self.output = 0
+    self.output = []
+    self.roll_id = Dice.roll_id
+    Dice.roll_id = Dice.roll_id + 1
 
   def __str__(self):
+    index = 0
     if (self.die == 6):
-      self.output = random.randrange(1, 7)
+      while (index < self.numDie):
+        self.output.append(random.randrange(1, 7))
+        index = index + 1
     return str(self.output)
-    
--- main.py
+++ main.py
@@ -5,7 +5,41 @@ Dice Rolling Application
 """
 from dice import Dice
 
-numDieInput = int(input("input number of dice: "))
-dieNumInput = int(input("input die number: "))
-dice = Dice(numDieInput, dieNumInput)
-print(dice)
+def roll():
+  print("Roll a Dice!\n\
+  For more information on how to roll a dice, check out the help page.")
+  dieInput = input("Input: ")
+  splitInput = dieInput.split("d")
+  dice = Dice(int(splitInput[0]), int(splitInput[1]))
+  print(dice)
+
+print("Dice Roller\n\
+Enter the number corresponding with what you would like to do\n\
+\t1: Roll Dice\n\
+\t2: View Previous Rolls\n\
+\t3: Help\n\
+\t4: Quit")
+welcome = int(input("Input: "))
+
+while (welcome != 4):
+  if (welcome == 1):
+    roll()
+  elif (welcome == 3):
+    print("To roll a dice, input the dice type. Dice types:\n\
+\t1d4: 4 Sided Die\n\
+\t1d6: 6 Sided Die\n\
+\t1d8: 8 Sided Die\n\
+\t1d9: 9 Sided Die\n\
+\t1d12: 12 Sided Die\n\
+\t1d10: 10 Sided Die\n\
+\t1s20: 20 Sided Die\n\
+\t1d100: 100 Sided Die\n\
+To roll multiple dice at a time, change the number before the d to how many dice you wish to roll.")
+  
+  print("Dice Roller\n\
+Enter the number corresponding with what you would like to do\n\
+\t1: Roll Dice\n\
+\t2: View Previous Rolls\n\
+\t3: Help\n\
+\t4: Quit")
+  welcome = int(input("Input: "))
